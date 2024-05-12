e95214bd39d5c16b6e965d2d7c073a63c29e9d75
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-11 23:11:14 -0500

    Clase GestorEmpleado Modificaciones

--- src/main/java/org/jhonatan/app/Models/GestorEmpleado.java
+++ src/main/java/org/jhonatan/app/Models/GestorEmpleado.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.jhonatan.app.Models;
 
 /**
  *
- * @author User
+ * @author Jhonatan
  */
 public class GestorEmpleado {
-    
+
+    private final Empleado vectorEmpleado[];
+    private int totalElementos;
+
+    public GestorEmpleado(int limite) {
+        this.vectorEmpleado = new Empleado[limite];
+        this.totalElementos = 0;
+    }
+
+    public GestorEmpleado() {
+        this.vectorEmpleado = new Empleado[100];
+        this.totalElementos = 0;
+    }
+
+    public Empleado obtenerEmpleado(int indice) {
+        return this.vectorEmpleado[indice];
+    }
+
+    public boolean agregarEmpleado(Empleado empleado) {
+        if (totalElementos < this.vectorEmpleado.length) {
+            this.vectorEmpleado[totalElementos] = empleado;
+            this.totalElementos++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean agregarEmpleado(String nombre, String cargo) {
+        if (totalElementos < vectorEmpleado.length) {
+            this.vectorEmpleado[totalElementos] = new Empleado(nombre, cargo);
+            totalElementos++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean agregarEmpleado(String nombre, String cargo, double salario) {
+        if (totalElementos < vectorEmpleado.length) {
+            this.vectorEmpleado[totalElementos] = new Empleado(nombre, cargo, salario);
+            totalElementos++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
